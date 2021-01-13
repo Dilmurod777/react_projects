3e068161606a92cafb1109b60a2a0949163ba17f
Dilmurod777 <valiev.dilmurod1998@gmail.com>
2021-01-14 00:51:42 +0500

    remove projects

--- api-console/src/store/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from "redux";
-import auth_reducer from "./auth-reducer";
-import console_reducer from "./console-reducer";
-
-export default combineReducers({
-  auth: auth_reducer,
-  console: console_reducer
-})
